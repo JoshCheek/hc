3878531e55abdb31b5c5d85566f714d409fbe128
Josh Cheek <josh.cheek@gmail.com>
2015-09-15 18:44:12 -0600

    Extract CSV parsing into its own class, add GraduationRate

--- lib/headcount.rb
+++ lib/headcount.rb
@@ -1,19 +1,55 @@
 require 'csv'
+
+class ParseCsv
+  attr_accessor :data_dir
+
+  def initialize(data_dir)
+    self.data_dir = data_dir
+  end
+
+  def parse
+    @repo_data ||= Hash.new.tap do |repo_data|
+      parse_pupil_enrollments repo_data
+      parse_graduation_rates  repo_data
+    end
+  end
+
+  def parse_pupil_enrollments(repo_data)
+    csv_data_from("Pupil enrollment.csv")
+      .group_by { |e| e[:location] }
+      .each { |district_name, rows|
+        district_for(repo_data, district_name)[:enrollment][:pupil] =
+          rows.map { |row| [row[:timeframe].to_i, row[:data].to_i] }.to_h
+      }
+  end
+
+  def parse_graduation_rates(repo_data)
+    csv_data_from('High school graduation rates.csv')
+      .group_by { |e| e[:location] }
+      .each { |district_name, rows|
+        district_for(repo_data, district_name)[:enrollment][:graduation_rate][:for_high_school_in_year] =
+          rows.map { |row| [row[:timeframe].to_i, row[:data].to_f] }.to_h
+      }
+  end
+
+  def district_for(repo_data, district_name)
+    repo_data[district_name] ||= {
+      enrollment: {
+        graduation_rate: {}
+      }
+    }
+  end
+
+  def csv_data_from(filename)
+    filename = File.join data_dir, filename
+    CSV.read(filename, headers: true, header_converters: :symbol).map(&:to_h)
+  end
+end
+
+
 class DistrictRepository
   def self.from_csv(data_dir)
-    repo_data   = {}
-
-    # enrollments
-    filename    = File.join data_dir, "Pupil enrollment.csv"
-    enrollments = CSV.read(filename, headers: true, header_converters: :symbol).map { |row| row.to_h }
-    enrollments.group_by { |e| e[:location] }
-               .each { |district_name, rows|
-                 district_data   = (repo_data[district_name] ||= {})
-                 enrollment_data = (district_data[:enrollment] ||= {})
-                 enrollment_data[:pupil] = rows.map { |row| [row[:timeframe].to_i, row[:data].to_i] }.to_h
-               }
-
-    DistrictRepository.new repo_data
+    new ParseCsv.new(data_dir).parse
   end
 
   attr_reader :districts
@@ -37,8 +73,11 @@ class District
 end
 
 class Enrollment
+  attr_reader :graduation_rate
+
   def initialize(data)
-    @pupil_data = data[:pupil]
+    @graduation_rate = GraduationRate.new data[:graduation_rate]
+    @pupil_data      = data[:pupil]
   end
 
   def in_year(year)
@@ -46,3 +85,12 @@ class Enrollment
   end
 end
 
+class GraduationRate
+  def initialize(data)
+    @data = data
+  end
+
+  def for_high_school_in_year(year)
+    @data.fetch(:for_high_school_in_year).fetch(year)
+  end
+end
