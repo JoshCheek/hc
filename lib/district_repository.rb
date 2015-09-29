21ab0d463edb0a046ca34b36a3e65a6d260fbfe7
Josh Cheek <josh.cheek@gmail.com>
2015-09-29 17:04:28 -0600

    parses api for district name

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,9 +1,39 @@
 require 'parse_csv'
 require 'district'
+require 'json'
+require 'rest-client'
+require 'pry'
+
+class ParseJson
+
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+  end
+
+  def parse
+    @data = JSON.parse(@data, :symbolize_names=>true)
+    @data = data.map do |district_name, data|
+      [district_name.to_s, data]
+    end.to_h
+
+    data.each do |district_name, data|
+      data[:testing] = data.delete :statewide_testing
+    end
+    @data
+  end
+end
+
 
 class DistrictRepository
   def self.from_csv(data_dir)
     new ParseCsv.new(data_dir).parse
+  end
+
+  def self.from_api(url)
+    data = RestClient.get url
+    new ParseJson.new(data).parse
   end
 
   attr_reader :districts_by_name
